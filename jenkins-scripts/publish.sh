592ef15f22b0b268c54bc130190762bb664c685a
Manoj Kumar Maharana <41284266+manoj016@users.noreply.github.com>
2021-01-18 14:02:10 +0530

    PLT-3952 #comment Added curl failure code (#19)

--- jenkins-scripts/publish.sh
+++ jenkins-scripts/publish.sh
@@ -7,4 +7,19 @@ set -ex
 echo "$BRANCH_NAME"
 
 make build
-make publish
+
+
+NAME=djangosaml2
+VERSION=$(awk '$1 == "__version__" {print $NF}' ./djangosaml2/_version.py | sed "s/'//g")
+OS=none
+CPU_ARCH=any
+
+WHEEL_FILENAME="$NAME-$VERSION-py3-$OS-$CPU_ARCH.whl"
+CODE=$(curl -sS -w '%{http_code}' -F package="@dist/$WHEEL_FILENAME" -o output.txt "https://$GEMFURY_AUTH_TOKEN@push.fury.io/quartic-ai/")
+cat output.txt && rm -rf output.txt
+if [[ "$CODE" =~ ^2 ]]; then
+    echo "$WHEEL_FILENAME Package published successfully"
+else
+    echo "ERROR: server returned HTTP code $CODE"
+    exit 1
+fi
